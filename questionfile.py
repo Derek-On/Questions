eeb605e1b56d6f565b631f6bda7b5ae10129e6c9
NPCWill <88618990+NPCWill@users.noreply.github.com>
2021-08-12 11:42:56 -0300

    Revert "Added the leg_counter function to questionfile.py"

--- questionfile.py
+++ questionfile.py
@@ -19,27 +19,3 @@
 #50 legs
 
 #Create a python script to solve this problem.
-
-#Defined Leg counter function
-def leg_counter():
-	#define legs variable
-	legs = 0
-	#Create chickens, cows, and dogs variables using user input
-	chickens = input("How many chickens? ")
-	cows = input("How many cows? ")
-	dogs = input("How many dogs? ")
-	#Checks to make sure a integer is entered for each variable. raising an exception if the value of the variable is not a number.
-	try:
-		#Multiplies each variable by the number of legs that creature has, adding the result to the legs variable
-		legs += 2*int(chickens)
-		legs += 4*int(cows)
-		legs += 4*int(dogs)
-	except ValueError:
-		#Tells the user that their input is invalid, and what a valid input looks like
-		print("Only numbers are valid, and a number must be entered for each animal")
-	#Returns a string containing the number of legs.			
-	return "{} legs".format(legs)
-
-#Runs the leg_counter function, printing the returned variable.
-print(leg_counter())
-
